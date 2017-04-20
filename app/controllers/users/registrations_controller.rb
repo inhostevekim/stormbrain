1aa86408dc710b764c4935be08ac31b8547f76ab
Steve Kim <inhostevekim@gmail.com>
2017-04-20 08:24:44 +0000

    modified registrations, still figuring out how site-stripe communication is not working

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,7 +6,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
         super do |resource|
             if params[:plan]
                 resource.plan_id = params[:plan]
-                if resource.plan.id == 2
+                if resource.plan.id == '2'
                     resource.save_with_payment
                 else
                     resource.save
